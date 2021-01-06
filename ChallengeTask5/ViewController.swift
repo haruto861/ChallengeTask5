a5c5e8336f15cc4769d070d9bf094d7a84cdd4ac
akio0911 <akio0911@gmail.com>
2021-01-06 22:53:35 +0900

    feedback

--- ChallengeTask5/ViewController.swift
+++ ChallengeTask5/ViewController.swift
@@ -9,37 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var resultLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
     @IBAction func didTapButton(_ sender: Any) {
         // 変数を最後に使用する為guard文
         guard let text1 = textField1.text, let number1 = Int(text1) else {
-            let alert = UIAlertController(title: "課題5", message: "割られる数を入れてください", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-                      return
-        }
-        guard let text2 = textField2.text, let number2 = Int(text2) else {
-            let alert2 = UIAlertController(title: "課題5", message: "割る数を入れてください", preferredStyle: .alert)
-            alert2.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert2, animated: true, completion: nil)
+            presentAlert(message: "割られる数を入れてください")
             return
         }
-        guard number2 == 0 else {
 
+        guard let text2 = textField2.text, let number2 = Int(text2) else {
+            presentAlert(message: "割る数を入れてください")
+            return
+        }
+
+        guard number2 == 0 else {
             resultLabel.text = "\(Double(number1) / Double(number2))"
             return
         }
 
-        let alert3 = UIAlertController(title: "課題5", message: "割る数には0を入れないでください", preferredStyle: .alert)
-        alert3.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert3, animated: true, completion: nil)
+        presentAlert(message: "割る数には0を入れないでください")
+    }
 
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
